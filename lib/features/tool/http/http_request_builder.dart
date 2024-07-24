713d8e7d7671ffaa863fa9adcda6116db7b0c63d
Sengthai Te <sengthai.te@amret.com.kh>
2024-07-24 09:51:00 +0700

    Update param table

--- lib/features/tool/http/http_request_builder.dart
+++ lib/features/tool/http/http_request_builder.dart
@@ -1,19 +1,129 @@
 import 'package:flutter/material.dart';
 
+class HttpRowData {
+  bool isSelected;
+  final String? key;
+  final String? value;
+
+  HttpRowData(
+      {required this.isSelected, required this.key, required this.value});
+}
+
 class HttpRequestBuilder {
   final urlInputController = TextEditingController();
   final authInputController = TextEditingController();
 
-  List<Map<bool, Map<String, TextEditingController>>> paramControllers = [];
-  List<Map<bool, Map<String, TextEditingController>>> headerControllers = [];
+  bool? selectedAllParam = true;
+  bool? selectedAllHeader = true;
 
-  addParam(Map<bool, Map<String, TextEditingController>> data) =>
-      paramControllers.add(data);
+  List<HttpRowData> paramControllers = [];
+  List<HttpRowData> headerControllers = [];
+
+  setParamSelectedRowAt(int index) {
+    paramControllers[index].isSelected = true;
+    var shouldSelectAllParam = true;
+    for (var row in paramControllers) {
+      if (!row.isSelected) {
+        shouldSelectAllParam = false;
+        break;
+      }
+    }
+    selectedAllParam = shouldSelectAllParam;
+  }
+
+  setParamUnSelectedRowAt(int index) {
+    selectedAllParam = false;
+    paramControllers[index].isSelected = false;
+  }
+
+  toggleParamAllRow() {
+    if (selectedAllParam == null) return;
+    selectedAllParam! ? deSelectParamAllRow() : selectParamAllRow();
+  }
+
+  toggleParamRowSelectionAt(int index) {
+    paramControllers[index].isSelected = !paramControllers[index].isSelected;
+    var shouldSelectAllParam = true;
+    for (var row in paramControllers) {
+      if (!row.isSelected) {
+        shouldSelectAllParam = false;
+        break;
+      }
+    }
+    selectedAllParam = shouldSelectAllParam;
+  }
+
+  selectParamAllRow() {
+    selectedAllParam = true;
+    for (var row in paramControllers) {
+      if (row.isSelected) continue;
+      row.isSelected = true;
+    }
+  }
+
+  deSelectParamAllRow() {
+    selectedAllParam = false;
+    for (var row in paramControllers) {
+      if (!row.isSelected) continue;
+      row.isSelected = false;
+    }
+  }
+
+  addParam(HttpRowData data) => paramControllers.add(data);
 
   removeParamAt(int index) => paramControllers.removeAt(index);
 
-  addHeader(Map<bool, Map<String, TextEditingController>> data) =>
-      headerControllers.add(data);
+  setHeaderSelectedRowAt(int index) {
+    headerControllers[index].isSelected = true;
+    var shouldSelectAllHeader = true;
+    for (var row in headerControllers) {
+      if (!row.isSelected) {
+        shouldSelectAllHeader = false;
+        break;
+      }
+    }
+    selectedAllHeader = shouldSelectAllHeader;
+  }
+
+  setHeaderUnSelectedRowAt(int index) {
+    headerControllers[index].isSelected = false;
+    selectedAllHeader = false;
+  }
+
+  toggleHeaderAllRow() {
+    if (selectedAllHeader == null) return;
+    selectedAllHeader! ? deSelectHeaderAllRow() : selectHeaderAllRow();
+  }
+
+  toggleHeaderRowSelectionAt(int index) {
+    headerControllers[index].isSelected = !headerControllers[index].isSelected;
+    var shouldSelectAllHeader = true;
+    for (var row in headerControllers) {
+      if (!row.isSelected) {
+        shouldSelectAllHeader = false;
+        break;
+      }
+    }
+    selectedAllHeader = shouldSelectAllHeader;
+  }
+
+  selectHeaderAllRow() {
+    selectedAllHeader = true;
+    for (var row in headerControllers) {
+      if (row.isSelected) continue;
+      row.isSelected = true;
+    }
+  }
+
+  deSelectHeaderAllRow() {
+    selectedAllHeader = false;
+    for (var row in headerControllers) {
+      if (!row.isSelected) continue;
+      row.isSelected = false;
+    }
+  }
+
+  addHeader(HttpRowData data) => headerControllers.add(data);
 
   removeHeaderAt(int index) => headerControllers.removeAt(index);
 
--- lib/features/tool/http/http_utils/http_util_param_view.dart
+++ lib/features/tool/http/http_utils/http_util_param_view.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:sengthaite_blog/features/tool/http/http_request_builder.dart';
 
-class HttpUtilParamView extends StatelessWidget {
+class HttpUtilParamView extends StatefulWidget {
   const HttpUtilParamView({super.key, required this.requestBuilder});
 
   final HttpRequestBuilder requestBuilder;
 
   @override
+  State<HttpUtilParamView> createState() => _HttpUtilParamViewState();
+}
+
+class _HttpUtilParamViewState extends State<HttpUtilParamView> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: DataTable(
+          columns: [
+            DataColumn(
+              label: Checkbox(
+                  value: widget.requestBuilder.selectedAllParam,
+                  onChanged: (value) {
+                    widget.requestBuilder.toggleParamAllRow();
+                    setState(() {
+                      widget.requestBuilder.selectedAllParam = value;
+                    });
+                  }),
+            ),
+            const DataColumn(
+                label: Text("Key",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14))),
+            const DataColumn(
+                label: Text("Value",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14))),
+            const DataColumn(
+                label: Text("Description",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14))),
+            const DataColumn(label: SizedBox(width: 50)),
+          ],
+          rows: List.generate(widget.requestBuilder.paramControllers.length,
+              (index) {
+            return DataRow(cells: []);
+          })),
+    );
   }
 }
