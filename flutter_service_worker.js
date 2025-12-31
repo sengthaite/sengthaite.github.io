5dfeb7da7ca6f11e0de7eb66a75a6e51b44913fd
Sengthai Te <87897249+sengthaite@users.noreply.github.com>
2025-12-31 14:40:17 +0700

    Built web
    Branch: main
    Commit: 508c3013e4974327c4b7209288a573e4f07e3cb7 (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d9b255f67951f6c9ef11ad4ef2b8e906",
+const RESOURCES = {"flutter_bootstrap.js": "b0c159829889ac81c60a40ded8602211",
 "version.json": "a07d8bd594cce6033c300d9d89d41de4",
 "index.html": "f7bf9dec00b417d3e9b582fab0d61b26",
 "/": "f7bf9dec00b417d3e9b582fab0d61b26",
-"main.dart.js": "cf69c40036fd48e4658a68497a5b1225",
+"main.dart.js": "d7ac795a9c3d1d4bc9144e80ce09c28e",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "icons/Icon-192.png": "e66d6bdfd9c8eac2471d5837a47f942f",
 "icons/Icon-maskable-192.png": "e66d6bdfd9c8eac2471d5837a47f942f",
@@ -96,7 +96,7 @@ const RESOURCES = {"flutter_bootstrap.js": "d9b255f67951f6c9ef11ad4ef2b8e906",
 "assets/assets/contents/os/minix-kernel-roadmap.txt": "168c6d12e7f8a29d76fd393759f86882",
 "assets/assets/contents/cambodia/name_of_provinces_cambodia.txt": "bced0089510ea7f9b6cd65c746da0562",
 "assets/assets/contents/tool/developer-cli-tools.txt": "34d6618a1b6f58c8717d53adbf61cf17",
-"assets/assets/autogen_meta/data_content.json": "8ecd7cb82dc61ae55ecd6c89b9271724",
+"assets/assets/autogen_meta/data_content.json": "20b6517b58013af5dd1d8b9f400e9ca9",
 "assets/assets/tool_icons/http.svg": "c51c16a6bd5185f37ccde7286a5bd1ad",
 "assets/assets/tool_icons/qrcode.png": "ed949df50843628bb449ec175ed0e20a",
 "assets/assets/tool_icons/camera.svg": "f42876ed16cc9e9137142675c5ad4b55",
