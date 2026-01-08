d438e74766f9ba2672f4ad3f84dd3d2d647d8419
sengthaite <sengthaite@users.noreply.github.com>
2026-01-08 15:13:12 +0000

    deploy: 362338eeb0a7cb46acf5bf233389687a2b1308b7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4185095423"
+    serviceWorkerVersion: "2670816028"
   }
 });
