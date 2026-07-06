4a26053f1c3da0de3ce7c48737c85acc82386c56
Sengthai Te <sengthaite@gmail.com>
2026-07-06 20:16:19 +0700

    downgrade file picker to build apk

--- lib/extensions/file_picker.dart
+++ lib/extensions/file_picker.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 
 Future<String?> openFilePicker() async {
   {
-    FilePickerResult? result = await FilePicker.pickFiles();
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
     if (result != null) {
       final fileContent = await result.files.single.xFile.readAsString();
       return fileContent;
